497a76a41c7a02b79e9c44e1a5128acbeb0e9227
Praveen-Kumar-Goswami <praveenkumarg8e8@gmail.com>
2025-08-05 13:10:21 +0530

    Add files via upload

---
+++ MIC/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import sounddevice as sd
+import threading
+
+app = Flask(__name__)
+is_muted = False
+
+samplerate = 48000
+blocksize = 128
+channels = 1
+
+def audio_loop():
+    global is_muted
+    def callback(indata, outdata, frames, time, status):
+        if is_muted:
+            outdata.fill(0)
+        else:
+            outdata[:] = indata
+    with sd.Stream(samplerate=samplerate, blocksize=blocksize,
+                   dtype='float32', channels=channels,
+                   callback=callback, latency='low'):
+        threading.Event().wait()  # Keeps thread alive
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/toggle', methods=['POST'])
+def toggle():
+    global is_muted
+    is_muted = not is_muted
+    return ('', 204)
+
+if __name__ == '__main__':
+    threading.Thread(target=audio_loop, daemon=True).start()
+    app.run(debug=False)
